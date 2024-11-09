43052e0238d357f988bab9e4abcd891195882d35
Ryan <reevesrs24@gmail.com>
2024-11-09 18:21:41 -0500

    Added ASCII Mapping Generation

--- prefix_mapper.py
+++ prefix_mapper.py
@@ -1,3 +1,5 @@
+import random
+
 from collections import defaultdict
 from typing import List, Tuple
 from datasets import load_dataset
@@ -6,8 +8,20 @@ class PrefixMapper:
     def __init__(self) -> None:
         # Initialize a default dictionary to store the count of prefixes
         self.prefix_map: defaultdict[str, int] = defaultdict(int)
+        self.prefix_list: List[int]
+        self.ascii_map: defaultdict[str, str]
 
-    def create_prefix_map(self) -> None:
+    def generate_mapping(self, key: str) -> None:
+        self._generate_prefix_map()
+        self._map_prefixes_to_ascii(key)
+
+    def _map_prefixes_to_ascii(self, key: str) -> None:
+        random.seed(key)
+        random.shuffle(self.prefix_list)
+
+        self.ascii_map = {x:prefix for x, prefix in zip(range(0, 256), self.prefix_list)}
+
+    def _generate_prefix_map(self) -> None:
         """
         Creates a prefix map from the 'wikitext-2-v1' dataset.
         The map counts occurrences of 1-character and 2-character
@@ -31,14 +45,13 @@ class PrefixMapper:
                         self.prefix_map[word[:2]] += 1
 
         # Sort the prefixes by their count in descending order and get the top 2 most frequent
-        top_prefixes: List[Tuple[str, int]] = sorted(self.prefix_map.items(), key=lambda x: x[1], reverse=True)[:100]
-        print(top_prefixes)
-
-
+        self.prefix_map: List[Tuple[str, int]] = sorted(self.prefix_map.items(), key=lambda x: x[1], reverse=True)[:256]
+        self.prefix_list = [x[0] for x in self.prefix_map]
+        
 
 def main():    
     pm = PrefixMapper()
-    pm.create_prefix_map()
+    pm.generate_mapping(key="abcde")
 
 if __name__ == '__main__':
     main()
